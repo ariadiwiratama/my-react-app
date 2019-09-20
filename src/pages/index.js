7e0d8bd829fa8b6b916f5869cd2fec470dd1dd57
unknown <you@example.com>
2019-09-21 02:43:48 +0800

    version 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,74 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Card from "../components/Card";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React app</h1>
-    <p>This is my first portfolio.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn Complete <br />Developing Courses</h1>
+        <p>Join the simple course now.</p>
+        <Link to="/page-2/">Watch The Video</Link>
+        <div className="Logos">
+          <img src={require('../images/gatsby-icon.png')}
+          width="50" />
+          <img src={require('../images/logo-figma.png')}
+          width="50" />
+          <img src={require('../images/logo-sketch.png')}
+          width="50" />
+          <img src={require('../images/logo-framer.png')}
+          width="50" />
+          <img src={require('../images/logo-studio.png')}
+          width="50" />          
+          <img src={require('../images/logo-swift.png')}
+          width="50" />                
+        </div>
+        <svg width="100%" height="448" viewBox="0 0 2560 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill="white">
+            <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+            
+            M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
+
+            M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
+
+            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
+            "
+            />
+          </path>
+        </svg>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Cards">
+      <h2>6 courses, more coming</h2>
+      <div className="CardGroup">
+        <Card 
+          title="React for Designers"
+          text="12 sections"
+          image={require('../images/wallpaper8.jpg')} />
+        <Card 
+          title="Javascript for Beginner"
+          text="11 sections"
+          image={require('../images/wallpaper9.jpg')} />
+        <Card 
+          title="Basic HTML+CSS"
+          text="5 sections"
+          image={require('../images/wallpaper10.jpg')} />
+        <Card 
+          title="Learn Java"
+          text="7 sections"
+          image={require('../images/wallpaper11.jpg')} />
+        <Card 
+          title="Learn Vue Basic"
+          text="8 sections"
+          image={require('../images/wallpaper12.jpg')} />
+        <Card 
+          title="Learn Laravel Advanced"
+          text="15 sections"
+          image={require('../images/wallpaper13.jpg')} />                    
+      </div>  
+    </div>
   </Layout>
 )
 
