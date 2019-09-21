8002eb7ee780d16fcd0852802be8c260ceaa7e45
unknown <you@example.com>
2019-09-22 01:32:52 +0800

    deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,35 +8,11 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve:'gatsby-source-contentful',
-      option: {
-        spaceId: '2v2q7vr052zd',
-        accessToken: 'SQGBKQ9X3MeLRD5Hj2iMz105ZeKCKAiPp_cjm_QiGys'
-      }
-    }
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `docs`,
+        path: `${__dirname}/../docs/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
